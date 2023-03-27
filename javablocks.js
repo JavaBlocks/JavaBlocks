9218058e6d5196030854534d9b6c51fbad6a117c
TylerMS887 <115214762+TylerMS887@users.noreply.github.com>
2023-03-27 07:59:23 +0100

    Update javablocks.js

--- javablocks.js
+++ javablocks.js
@@ -38,7 +38,7 @@ var workspace = Blockly.inject(options);
 /* Load Workspace Blocks from XML to workspace. Remove all code below if no blocks to load */
 
 /* TODO: Change workspace blocks XML ID if necessary. Can export workspace blocks XML from Workspace Factory. */
-var workspaceBlocks = document.getElementById("workspaceBlocks"); 
+var workspace = Blockly.inject(/* TODO: Add ID of div to inject Blockly into */, options);
 
 /* Load blocks to workspace. */
 Blockly.Xml.domToWorkspace(workspaceBlocks, workspace);
